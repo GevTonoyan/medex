0a17a0da29f97fbf578d796aff9e956ad08cad2f
Gevorg <yourgitemail@example.com>
2024-10-20 13:15:52 +0400

    Fix main offers fetch refresh

--- lib/ui/main/views/main_offers_widget.dart
+++ lib/ui/main/views/main_offers_widget.dart
@@ -22,75 +22,84 @@ class _MainOffersWidgetState extends State<MainOffersWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          //TODO make page view with expanded sizes
-          height: uiOrientedSwitch(110, 144),
-          width: double.maxFinite,
-          child: PageView(
-            controller: pageController,
-            onPageChanged: (int index) {
-              setState(() {
-                selectedIndex = index;
-              });
-            },
-            children: [
-              for (final offer in offersModel.offers) ...[
-                Text(
-                  offer.translatedTitle(),
-                  style: Configuration().isMobile ? AppFonts.titleMobile : AppFonts.titleDesktop,
-                  maxLines: 3,
-                ),
-              ],
-            ],
-          ),
-        ),
-        SizedBox(height: uiOrientedSwitch(30, 24)),
-        DefaultButton2(
-          label: 'main_see_more'.tr,
-          size: Size(uiOrientedSwitch(double.maxFinite, 187), 52),
-          onPressed: () {},
-        ),
-        SizedBox(height: uiOrientedSwitch(30, 80)),
-        Container(
-          padding: EdgeInsets.only(left: 8),
-          height: uiOrientedSwitch(10, 16),
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: offersModel.offers.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Row(
-                children: [
-                  Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      child: Container(
-                        height: uiOrientedSwitch(10, 16),
-                        width: uiOrientedSwitch(10, 16),
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: AppColors.primary),
-                          color: selectedIndex == index ? AppColors.primary : AppColors.primary.withOpacity(0.2),
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                      onTap: () {
-                        pageController.animateToPage(
-                          index,
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.easeInOut,
-                        );
-                      },
-                    ),
+    return Obx(() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            //TODO make page view with expanded sizes
+            height: uiOrientedSwitch(110, 144),
+            width: double.maxFinite,
+            child: PageView(
+              controller: pageController,
+              onPageChanged: (int index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              children: [
+                for (final offer in offersModel.offers) ...[
+                  Text(
+                    offer.translatedTitle(),
+                    style: Configuration().isMobile
+                        ? AppFonts.titleMobile
+                        : AppFonts.titleDesktop,
+                    maxLines: 3,
                   ),
-                  SizedBox(width: uiOrientedSwitch(6, 10)),
                 ],
-              );
-            },
+              ],
+            ),
           ),
-        )
-      ],
-    );
+          SizedBox(height: uiOrientedSwitch(30, 24)),
+          DefaultButton2(
+            label: 'main_see_more'.tr,
+            size: Size(uiOrientedSwitch(double.maxFinite, 187), 52),
+            onPressed: () {},
+          ),
+          SizedBox(height: uiOrientedSwitch(30, 80)),
+          Container(
+            padding: EdgeInsets.only(left: 8),
+            height: uiOrientedSwitch(10, 16),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: offersModel.offers.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Row(
+                  children: [
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        highlightColor: Colors.transparent,
+                        splashColor: Colors.transparent,
+                        child: Container(
+                          height: uiOrientedSwitch(10, 16),
+                          width: uiOrientedSwitch(10, 16),
+                          decoration: BoxDecoration(
+                            border:
+                                Border.all(width: 1, color: AppColors.primary),
+                            color: selectedIndex == index
+                                ? AppColors.primary
+                                : AppColors.primary.withOpacity(0.2),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        onTap: () {
+                          pageController.animateToPage(
+                            index,
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.easeInOut,
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(width: uiOrientedSwitch(6, 10)),
+                  ],
+                );
+              },
+            ),
+          )
+        ],
+      );
+    });
   }
 }
